d46e0c5a1d59bb2ddd1302967ab4190c58e1d4c9
sky <skylerseifert@gmail.com>
2024-11-15 13:04:46 -0800

    chore: update main file with base example

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"go-zigbee-herdsman/internal/controller"
+	"go-zigbee-herdsman/internal/transport"
+	"time"
+
+	"github.com/tarm/serial"
+)
+
 func main() {
+	transport := transport.NewSerialPort(&serial.Config{
+		Name:        "/dev/ttyAMA0",
+		Baud:        38400,
+		ReadTimeout: time.Second,
+	})
+
+	controller := controller.NewController(transport)
+	controller.Start()
 }
